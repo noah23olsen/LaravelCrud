2d586de8f7ee2613f03a152d20ece9ece09c253e
noah23olsen <noah23olsen@gmail.com>
2023-11-04 12:18:01 -0400

    hardcoded AND functional INSERT added to controller & routing

--- crud2-app/app/Http/Controllers/CarController.php
+++ crud2-app/app/Http/Controllers/CarController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
 
 class CarController extends Controller
 {
-    //
+    public function addCar(){
+        $car = new Car(); //create new instance of Car object
+        $car->car_name = 'Cybertruck'; //set name attribute of the object
+        //save() INSERT, update() UPDATE, delete() DELETE
+        $car->save(); //INSERT the car object to the database(triggers SQL insert)
+
+        return 'Car added successfully!';
+    }
 }
--- crud2-app/routes/web.php
+++ crud2-app/routes/web.php
@@ -1,18 +1,12 @@
 <?php
+//php artisan serve - runs code locally
+//artisan is a CLI for Laravel
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\CarController; //imports CarController
 
 Route::get('/', function () {
     return view('welcome');
 });
+//get request at the class CarController, method addCar
+Route::get('/addCar',[CarController::class,'addCar'] );
